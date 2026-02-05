4d2bd1757aae8f92a7cd8ace05ff06a99e731607
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-02-05 07:36:01 +0300

    [scripts] update config application script to support sudo password file

--- apply_config.sh
+++ apply_config.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-
-# Пути к зависимостям и конфигу
 CONFIG_DIR="/var/home/neg/.gemini/tmp/salt_config"
-
-# Проверка на наличие sudo (для изменения системных настроек)
-if [ "$EUID" -ne 0 ]; then
-  echo "Внимание: скрипт запущен не от root. Системные изменения (timezone, pkg, service) могут не сработать."
-  echo "Рекомендуется: sudo -E ./apply_config.sh $@"
-fi
-
-# Режим запуска
+SUDO_PASS=$(cat "/var/home/neg/src/salt/.password")
 ACTION="state.sls"
 STATE="system_description"
 
 if [[ "$1" == "--dry-run" ]]; then
   echo "--- Запуск в режиме тестирования (изменения не будут применены) ---"
-  python3.14 /var/home/neg/src/salt/run_salt.py \
+  $SUDO_CMD python3.14 /var/home/neg/src/salt/run_salt.py \
     --config-dir="${CONFIG_DIR}" \
     -l info \
     --local ${ACTION} ${STATE} test=True
 else
+  echo "SUDO_PASS=$SUDO_PASS"
   echo "--- Применение конфигурации ---"
-  python3.14 /var/home/neg/src/salt/run_salt.py \
+  echo "$SUDO_PASS" | sudo -S -E python3.14 /var/home/neg/src/salt/run_salt.py \
     --config-dir="${CONFIG_DIR}" \
     -l info \
     --local ${ACTION} ${STATE}
