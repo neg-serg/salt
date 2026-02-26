314c61083f4d91250864b3238024b16673ffbcfd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-02-26 06:03:01 +0300

    [zsh] optimize startup: cache eval outputs, lazy compinit, zcompile
    - Cache zoxide init output (invalidate on binary update)
    - Cache aliae init output by config hash (skip fork on every startup)
    - Replace md5sum|awk with parameter expansion (remove awk fork)
    - Replace tail|grep with pure zsh file read in fsyh-parser
    - Defer compinit to first Tab press (fish-style lazy init)
    - Background-compile .zsh modules and lazyfuncs digest to .zwc

--- dotfiles/dot_config/zsh/04-fzf.zsh
+++ dotfiles/dot_config/zsh/04-fzf.zsh
@@ -44,11 +44,16 @@ for f in key-bindings.zsh completion.zsh; do
   [[ -s "$dst" && ! "$src" -nt "$dst" ]] || cp -f -- "$src" "$dst"
 done
 
-# Make sure completion infra exists before fzf completion
-autoload -Uz compinit
-(( ${+_comps} )) || compinit -d "${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompdump"
-
-# Load (zsh will prefer compiled .zwc if present)
+# Load fzf scripts (no compinit needed at source time)
 source "${_fzf_cache_dir}/key-bindings.zsh" 2>/dev/null
 source "${_fzf_cache_dir}/completion.zsh"   2>/dev/null
-bindkey "^I" fzf-on-tab
+
+# Lazy compinit: defer initialization until first Tab press (fish-style trick)
+# This avoids ~15-20ms compinit cost when shell is used for a quick command without Tab
+_fzf_tab_lazy_init() {
+  _zpcompinit_custom
+  bindkey '^I' fzf-on-tab
+  zle fzf-on-tab
+}
+zle -N _fzf_tab_lazy_init
+bindkey '^I' _fzf_tab_lazy_init
