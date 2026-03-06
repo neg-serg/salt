80aa4bc0a3a530c1490779d631a78c4832473f80
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-03-06 16:36:56 +0300

    [tidal] add Ctrl+Enter keybinds for launch/quit
    Consistent modifier+Enter pattern across all tidal operations:
    C-CR launch, C-S-CR quit, S-CR send line, M-CR send block.

--- dotfiles/dot_config/nvim/lua/plugins/filetypes/tidal.lua
+++ dotfiles/dot_config/nvim/lua/plugins/filetypes/tidal.lua
@@ -4,6 +4,10 @@
 return {
     'grddavies/tidal.nvim',
     event = { 'BufRead *.tidal', 'BufNewFile *.tidal' },
+    keys = {
+        { '<C-CR>', '<Cmd>TidalLaunch<CR>', ft = 'haskell', desc = 'Launch Tidal + SuperDirt' },
+        { '<C-S-CR>', '<Cmd>TidalQuit<CR>', ft = 'haskell', desc = 'Quit Tidal' },
+    },
     opts = {
         boot = {
             tidal = {
