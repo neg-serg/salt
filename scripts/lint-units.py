7bb52455b7af5cc81ba17da80bf3ea3beca79dd0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-03-25 12:43:28 +0300

    [lint] ignore external unit executable warnings

--- scripts/lint-units.py
+++ scripts/lint-units.py
@@ -20,6 +20,8 @@ UNITS_DIR = os.path.join("states", "units")
 _NOISE = re.compile(
     r"is not executable: No such file|"
     r"No such file or directory|"
+    r"Configuration file .+ is marked executable\. "
+    r"Please remove executable permission bits\. Proceeding anyway\.|"
     r"ananicy-cpp\.service|"
     r"Failed to turn off SO_PASSRIGHTS on user lookup socket|"
     r"Failed to enable SO_PASSCRED on handoff timestamp socket"
